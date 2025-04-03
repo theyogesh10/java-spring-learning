5a75757dad5a445a55dc8d66d1b38385f9a8bcef
theyogesh10 <yogeshpahade10@gmail.com>
2025-04-03 22:31:57 +0530

    hello world through servlet

--- Day36-JavaEEHelloWorld/src/main/java/com/yogeshjava/learning/HelloWorldServlet.java
+++ Day36-JavaEEHelloWorld/src/main/java/com/yogeshjava/learning/HelloWorldServlet.java
@@ -3,6 +3,7 @@ package com.yogeshjava.learning;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,13 +24,30 @@ public class HelloWorldServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		String user = req.getParameter("userName");
-		String pass = req.getParameter("password");
+	
+    
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		System.out.println("get");
+		String user = req.getParameter("userName"); 
+		String pass = req.getParameter("password"); 
+		
+		UserVerification object = new UserVerification();
+		boolean result = object.isValid(user, pass);
 		PrintWriter out = res.getWriter();
+		if(result==true) {
+			out.println("Valid");
+		}
+		else {
+			out.println("Invalid");
+		}
+		
 		out.println("<h1 style='color:red'>Hello World through Servlet</h1>");
 		System.out.println(user + " " + pass);
 	}
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		System.out.println("post");
+	}
+	
 
 }
