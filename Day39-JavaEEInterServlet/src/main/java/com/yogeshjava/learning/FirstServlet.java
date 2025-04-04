767965c6078a100de79468694847e270420d2e56
theyogesh10 <yogeshpahade10@gmail.com>
2025-04-04 18:20:50 +0530

    inter-servlet communication

---
+++ Day39-JavaEEInterServlet/src/main/java/com/yogeshjava/learning/FirstServlet.java
@@ -0,0 +1,50 @@
+package com.yogeshjava.learning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Test");
+		String text = request.getParameter("test");
+		System.out.println(text);
+		text = text.toUpperCase();
+		request.setAttribute("upperName", text);
+		RequestDispatcher rd = request.getRequestDispatcher("second");
+		rd.include(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
